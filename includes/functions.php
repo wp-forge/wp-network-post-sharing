5adc686f84a393bd9681da42bcc627fc52dfe42f
Daryl Lozupone <daryl@clubdeuce.net>
2022-04-22 16:09:20 -0400

    Fix path to hooks directory

--- includes/functions.php
+++ includes/functions.php
@@ -1,6 +1,6 @@
 <?php
 // Automatically load all PHP files in the 'includes/hooks/' directory
-$iterator = new RecursiveDirectoryIterator( __DIR__ . '/includes/hooks' );
+$iterator = new RecursiveDirectoryIterator( __DIR__ . '/hooks' );
 foreach ( new RecursiveIteratorIterator( $iterator ) as $file ) {
     if ( $file->getExtension() === 'php' ) {
         require $file;
