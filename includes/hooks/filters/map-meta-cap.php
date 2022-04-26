d73c3e0daea5b17ab22be58582b96b2c3b1bfe9d
Daryl Lozupone <daryl@clubdeuce.net>
2022-04-26 16:01:40 -0400

    Super admins can see remote shares

--- includes/hooks/filters/map-meta-cap.php
+++ includes/hooks/filters/map-meta-cap.php
@@ -6,16 +6,22 @@
  */
 add_filter( 'map_meta_cap', function( $caps, $cap, $user_id, $args ) {
 
-//    if( in_array( $cap, array( 'edit_post', 'delete_post' ) ) && isset( $args[0] ) ) {
-//
-//        $post_id = $args[0];
-//
-//        if( get_post_meta( $post_id, '_origin_blog_id' ) ) {
-//            //This is a shared listing and therefore is not editable or trashable
-//            $caps[] = 'do_not_allow';
-//        }
-//
-//    }
+    do {
+        if ( current_user_can( 'create_sites' ) ) {
+            break;
+        }
+
+        if( in_array( $cap, array( 'edit_post', 'delete_post' ) ) && isset( $args[0] ) ) {
+
+            $post_id = $args[0];
+
+            if( get_post_meta( $post_id, '_origin_blog_id' ) ) {
+                //This is a shared listing and therefore is not editable or trashable
+                $caps[] = 'do_not_allow';
+            }
+
+        }
+    } while ( false );
 
     return $caps;
 
