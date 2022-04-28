e66bfffc8f3216cd786b226fbba4f48ca0e9293e
Daryl Lozupone <daryl@clubdeuce.net>
2022-04-27 22:03:53 -0400

    Resolve infinite loop

--- includes/hooks/actions/delete-post.php
+++ includes/hooks/actions/delete-post.php
@@ -1,9 +1,14 @@
 <?php
-add_action( 'before_delete_post', function ( $post_id ) {
+add_action( 'before_delete_post', 'wpfnps_before_delete_post' );
+
+function wpfnps_before_delete_post( $post_id ) {
 
     if ( in_array( get_post_type( $post_id ), wpfnps_post_types() ) ) {
-       wpfnps_delete_remote_shares( $post_id );
-       wpfnps_delete_origin_share_reference( $post_id );
+        remove_action( 'before_delete_post', 'wpfnps_before_delete_post' );
+        wpfnps_delete_remote_shares( $post_id );
+        wpfnps_delete_origin_share_reference( $post_id );
+        add_action( 'before_delete_post', 'wpfnps_before_delete_post' );
     }
-} );
+
+}
 
