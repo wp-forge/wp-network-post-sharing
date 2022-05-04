e1af7883a89a94b533120420b34b40e929fd1956
Daryl Lozupone <daryl@clubdeuce.net>
2022-05-04 13:59:42 -0400

    Remove post association from origin post when trashing a remote share

--- includes/hooks/actions/trash-post.php
+++ includes/hooks/actions/trash-post.php
@@ -4,22 +4,10 @@ add_action( 'wp_trash_post', 'wpfnps_trash_post' );
 
 function wpfnps_trash_post( $post_id ) {
 
-	if ( in_array( get_post_type( $post_id ), wpfnps_post_types() ) ) {
-		$shared_with = wpfnps_get_shares( $post_id );
-
-		if ( is_array( $shared_with ) ) {
-			foreach ( $shared_with as $site_id ) {
-				$remote_id = get_post_meta( $post_id, "_remote_share_id_{$site_id}", 'single' );
-
-				if ( $remote_id ) {
-					remove_action( 'wp_trash_post', 'wpfnps_trash_post' );
-					switch_to_blog( $site_id );
-					wp_delete_post( $remote_id, "force delete" );
-					restore_current_blog();
-					add_action( 'wp_trash_post', 'wpfnps_trash_post' );
-				}
-			}
-		}
-	}
+    if ( in_array( get_post_type( $post_id ), wpfnps_post_types() ) ) {
+        remove_action( 'before_delete_post', 'wpfnps_before_delete_post' );
+        wpfnps_delete_remote_shares( $post_id );
+        wpfnps_delete_origin_share_reference( $post_id );
+    }
 
 }
